35ed8dd599237884eec9986e856196cb51ef9d92
SujithS <sjhece@gmail.com>
2026-08-17 15:24:09 +0530

    PullRequest3

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -8,3 +8,17 @@ test("Verify Textbox Actions",async({page})=>{
   await page.waitForTimeout(2000);
 
 });
+
+test("Verify Radio buttons",async({page})=>{
+  await page.goto("https://testautomationpractice.blogspot.com/");
+  
+  const maleradio=page.locator("css=#male");    //Male radio button locator
+  await expect(maleradio).toBeVisible();
+  await expect(maleradio).toBeEnabled();
+  await maleradio.check();                           //select radio button
+  await expect(maleradio).toBeChecked();            //verify radio button is selected
+  console.log("male radio button is selected");
+
+  await page.waitForTimeout(3000);
+
+});
